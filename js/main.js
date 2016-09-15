df1b76b6dfe757e6b2730905eebe80f9db993c13
Clic Art Creaciones Publicitarias <Clic Art Creaciones Publicitarias>
2016-09-15 12:26:18 -0500

    contraseña

--- js/main.js
+++ js/main.js
@@ -1,30 +1,20 @@
 function validateForm(){
 	var valor = document.getElementById("name").value;
-	if( valor == null || valor.length == 0 || /^\s+$/.test(valor) ) {
-    alert("Campo obligatorio");
-  }
-    else{
-        var m = /^[a-zA-Z]*$/;
-        if(!valor.search(m))
-               console.log("letra")
-               m=/[a-z]/g;
-               if(!valor.search(m)){
-                   alert("La primera letra debe empezar con maýuscula")
-               }
-          else
-             alert ("Ingrese datos válidos (A-Z)");
-    }
+	if( valor == null || valor.length == 0 || /^[a-zA-Z._.-]/.test(valor) ) {
+ 		 alert("Ingresa tu nombre la primera letra con mayuscula!");
+ 	}
 
 	var valorDos = document.getElementById("lastname").value;
 	if( valorDos == null || valorDos.length == 0 || /^[a-zA-Z._.-]/.test(valorDos) ) {
- 		 return alert("Faltan ingresar tu Apellido!");
+ 		 return alert("Ingresa tu Apellido la primera letra con mayuscula!");
 	}
 	var valorTres = document.getElementById("input-email").value;
 	if( valorTres == null || valorTres.length == 0 || !/^[0-9a-zA-Z._.-]+\@[0-9a-zA-Z._.-]+\.[0-9a-zA-Z]+$/.test(valorTres) ) {
  		 return alert("Faltan ingresar tu Email!");
 	}
 	var valorCuatro = document.getElementById("input-password").value;
-	if( valorCuatro == null || valorCuatro.length == 0 || /^\s+$/.test(valorCuatro) ) {
+    if( valorCuatro == null || valorCuatro.length == 0 || valorCuatro.length <= 6 || valorCuatro == "123456" || valorCuatro == "98754" || valorCuatro == "password") {
+        return alert("Contraseña no válida");
  		 return alert("Faltan ingresar tu contraseña!");
  		 } else if (valorCuatro.length < 6){
         return alert("tu contraseña debe tener más de 6 carácteres")
